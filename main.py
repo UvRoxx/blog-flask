e407d35b04acee4954423a600d8a57388b4e0855
Utkarsh Varma <uvroxx2323@gmail.com>
2021-02-16 03:47:20 -0500

    Added bootstrap to make the site more responsive and add a better UI and UX to the page

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from flask import Flask, render_template
 import requests
 import json
 from random import randint
-
+import datetime
 
 app = Flask(__name__)
 blog_endpoint = "https://api.npoint.io/5abcca6f4e39b4955965"
 
 
 @app.route('/post/<blog_id>')
-def show_post(blog_id):
+def post(blog_id):
     print(f"blog enfpoint is {blog_id}")
     response = requests.get(blog_endpoint)
     posts = json.loads(response.text)
@@ -20,11 +20,19 @@ def show_post(blog_id):
 
 @app.route('/')
 def home():
+    year = datetime.date.today().year
     response = requests.get(blog_endpoint)
     posts = json.loads(response.text)
     num = randint(2, 3)
-    return render_template("index.html", posts=posts, showcase_id=num)
+    return render_template("index.html", posts=posts, showcase_id=num, year=year)
 
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
